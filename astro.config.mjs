9042e28c5d527bb93140ac3e703f9e8977f91d04
Andy <andy.dabadie@gmail.com>
2026-06-01 12:45:45 -0400

    refactor: remove GitHub Pages adapter from configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import githubPages from '@astrojs/github-pages';
 
 export default defineConfig({
     site: 'https://sokey7.github.io',
     base: '/andy-portfolio-website',
     output: 'static',
-    adapter: githubPages(),
 });
