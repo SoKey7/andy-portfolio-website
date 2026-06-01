835a805c2ef276e74e81003d7ee0c4d8c73d595f
Andy <andy.dabadie@gmail.com>
2026-06-01 12:37:56 -0400

    feat: add GitHub Actions workflow for deployment to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import githubPages from '@astrojs/github-pages';
 
 export default defineConfig({
     site: 'https://sokey7.github.io',
     base: '/andy-portfolio-website',
+    output: 'static',
+    adapter: githubPages(),
 });
